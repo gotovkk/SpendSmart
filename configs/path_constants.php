87788b9d0d31e8d8cc5965e31fd29cbfee8d36b8
gotovkk <gotovkkd@gmail.com>
2024-10-13 20:51:16 +0300

    1

---
+++ configs/path_constants.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types = 1);
+
+const APP_PATH     = __DIR__ . '/../app';
+const VIEW_PATH    = __DIR__ . '/../resources/views';
+const STORAGE_PATH = __DIR__ . '/../storage';
+const CONFIG_PATH  = __DIR__ . '/../configs';
+const BUILD_PATH   = __DIR__ . '/../public/build';
