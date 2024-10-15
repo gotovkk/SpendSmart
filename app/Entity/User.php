d01a8cfb2ecbcaeb56107ca06dee14a38097a5d3
gotovkk <gotovkkd@gmail.com>
2024-10-15 23:28:43 +0300

    auth and password hash add

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -5,14 +5,19 @@ namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\PrePersist;
+use Doctrine\ORM\Mapping\PreUpdate;
 use Doctrine\ORM\Mapping\Table;
 
 #[Entity, Table('users')]
+#[HasLifecycleCallbacks]
 Class User {
 
     #[Id, Column(options: ['unsigned' => true]), GeneratedValue]
@@ -49,6 +54,15 @@ Class User {
     public function getId(): int
     {
         return $this->id;
+    }
+
+    #[PrePersist, PreUpdate]
+    public function updateTimestamps(LifecycleEventArgs $args): void
+    {
+        if( !isset($this->createdAt)) {
+            $this->createdAt = new \DateTime();
+        }
+        $this->updatedAt = new \DateTime();
     }
 
     public function getName(): string
